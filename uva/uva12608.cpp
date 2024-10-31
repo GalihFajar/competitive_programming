87875091c9ad0154e1279d6bafd3c2f10be95223
Galih Fajar <galihfajar00@gmail.com>
2024-11-01 06:22:07 +0700

    Solve uva12608

---
+++ uva/uva12608.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+typedef vector<pair<long, long>> vpii;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    long tc; cin >> tc;
+
+    while (tc--) {
+        long limit, n; cin >> limit >> n;
+        long max_in = n;
+        long total_distance = 0;
+        long current_weight = 0;
+        long last_distance_from_dump = 0;
+        vpii pickup_points;
+        int cnt = 0;
+
+        while (n--) {
+            long current_distance_from_dump, weight;
+            cin >> current_distance_from_dump >> weight;
+            cnt++;
+
+            total_distance += current_distance_from_dump - last_distance_from_dump;
+            current_weight += weight;
+
+            if (current_weight > limit) {
+                current_weight = weight;
+                total_distance += 2 * current_distance_from_dump;
+
+                if (cnt == max_in) {
+                    total_distance += current_distance_from_dump;
+                    break;
+                }
+
+                if (current_weight == limit) {
+                    if (cnt == max_in) {
+                        total_distance += current_distance_from_dump;
+                        break;
+                    }
+
+                    current_weight = 0;
+                    total_distance += 2 * current_distance_from_dump;
+                }
+            } else if (current_weight == limit) {
+                if (cnt == max_in) {
+                    total_distance += current_distance_from_dump;
+                    break;
+                }
+
+                current_weight = 0;
+                total_distance += 2 * current_distance_from_dump;
+            }
+
+            if (cnt == max_in) {
+                total_distance += current_distance_from_dump;
+            }
+
+            last_distance_from_dump = current_distance_from_dump;
+        }
+
+
+        cout << total_distance << '\n';
+    }
+
+    return 0;
+}
