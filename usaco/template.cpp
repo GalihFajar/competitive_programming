16a19ec3591f9990ec887735230e5ab75aa669a7
GalihFajar <galihfajar00@gmail.com>
2025-10-21 10:15:23 +0700

    Add template

---
+++ usaco/template.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <cmath>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+template <typename T> using v = vector<T>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using pii = pair<int, int>;
+
+#define all(x) begin(x), end(x)
+#define fio(name) freopen(name ".in", "r", stdin); freopen(name ".out", "w", stdout);
+#define psp(x) cout << x << " ";
+#define pnl(x) cout << x << "\n";
+
+template <typename T>
+void print_v(vector<T>& v) {
+    for (auto const& elem: v) {
+	cout << elem << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // fio("<filename_prefix>")
+    
+
+    return 0;
+}
+
+
+/*
+ *
+ *
+*/
